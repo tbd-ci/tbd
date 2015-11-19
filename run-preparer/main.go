b1562e6c6443245afd77aa4b998f211be53fc1c5
Jim Gaylard <jim.gaylard@rea-group.com>
2015-11-19 17:36:47 +1100

    Run-preparer prepares new index

--- run-preparer/main.go
+++ run-preparer/main.go
@@ -7,25 +7,32 @@ import (
 	"os/exec"
 )
 
-func createTempDir() (dir string) {
-	dir, err := ioutil.TempDir("/tmp", "tbd")
+func CheckoutProject(treeId string) (tmpDir string) {
+	tmpDir, err := ioutil.TempDir("/tmp", "tbd")
 	if err != nil {
 		panic(err)
+	}
+
+	if err := exec.Command("git", "--work-tree", tmpDir, "checkout", treeId, "--", ".").Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("git", "init")
+	cmd.Dir = tmpDir
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	cmd2 := exec.Command("git", "add", "-A")
+	cmd2.Dir = tmpDir
+	if err := cmd2.Run(); err != nil {
+		log.Fatal(err)
 	}
 
 	return
 }
 
-func CheckoutProject(projectDir string, treeId string) {
-	tmpDir := createTempDir()
-	cmd := fmt.Sprintf("git --git-dir=%s/.git --work-tree=%s checkout %s -f -q", projectDir, tmpDir, treeId)
-
-	_, err := exec.Command(cmd).Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
-	CheckoutProject("/work-space/work/rea/tbd-test-repo/", "HEAD")
+	tmpDir := CheckoutProject("HEAD")
+	fmt.Println(tmpDir)
 }
