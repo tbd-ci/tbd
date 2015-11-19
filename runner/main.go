b0060850b7afad82ef8efd582b570109ad359f77
☀ Prem Pillai (@premjg) <prem.pillai@gmail.com>
2015-11-19 11:58:55 +1100

    Adds CombinedOutput.

--- runner/main.go
+++ runner/main.go
@@ -9,16 +9,17 @@ import (
 )
 
 func main() {
-	a := runner("/work-space/work/rea/tbd-test-repo/")
+	a := Runner("/work-space/work/rea/tbd-test-repo/")
 	fmt.Println(a)
 }
 
 type success bool
 
 type ResultStep struct {
-	Success success `json:"status"`
-	StdErr  string  `json:"standardError"`
-	StdOut  string  `json:standardOut`
+	Success     success `json:"status"`
+	StdErr      string  `json:"standardError"`
+	StdOut      string  `json:standardOut`
+	CombinedStd string  `json:combinedStd`
 }
 
 type Result struct {
@@ -26,7 +27,7 @@ type Result struct {
 	Steps          []ResultStep `json:steps`
 }
 
-func runner(path string) (res Result) {
+func Runner(path string) (res Result) {
 	ciPath := path + "ci/"
 
 	for _, step := range steps(ciPath) {
@@ -36,18 +37,21 @@ func runner(path string) (res Result) {
 
 		rawCmd := ciPath + step.Name() + "/run"
 		cmd := exec.Command(rawCmd)
+
+		combinedStd, err := cmd.CombinedOutput()
 		cmd.Stdout = &stdOut
 		cmd.Stderr = &stdErr
 
-		err := cmd.Run()
 		if err != nil {
 			res.OverallSuccess = false
 			resultStep.Success = false
 			resultStep.StdErr = stdErr.String()
+			resultStep.CombinedStd = string(combinedStd)
 		} else {
 			fmt.Println(err)
 			resultStep.Success = true
 			resultStep.StdOut = stdOut.String()
+			resultStep.CombinedStd = string(combinedStd)
 		}
 
 		res.Steps = append(res.Steps, resultStep)
